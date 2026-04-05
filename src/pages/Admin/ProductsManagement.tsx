d26dc5b57e6c8be58bdebfcc074fae7c69f2a93d
master <masteremail.com>
2026-04-05 09:21:58 +0000

    fix: add React namespace import to ProductsManagement (fixes Vercel build)

--- src/pages/Admin/ProductsManagement.tsx
+++ src/pages/Admin/ProductsManagement.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import type { RootState } from "../../app/store";
 import LoadingLogo from "../../components/LoadingLogo";
